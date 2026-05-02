ba47e650f19adce453abd0c5568ff17acd22e89c
oriolmontcreus <oriolmontcreus@gmail.com>
2026-05-02 12:36:37 +0200

    chore: added shadcn-svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,25 @@
 // @ts-check
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 import { defineConfig } from 'astro/config';
 
 import svelte from '@astrojs/svelte';
 
 import tailwindcss from '@tailwindcss/vite';
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte()],
 
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        $lib: path.resolve(__dirname, 'src/lib'),
+      },
+    },
+  },
 });
